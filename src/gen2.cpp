7694ecd6b09f37bb7a3ec3cdbf447cda0b2d248d
John <John>
2016-06-13 00:12:22 +0100

    Null terminate strings and add initial table creation

--- src/gen2.cpp
+++ src/gen2.cpp
@@ -20,7 +20,7 @@ int main(void) {
       char buffer[BUFSIZ];
       char headings[100][100];
 	  int idx = 0;
-	  int headingNumber = 0;
+	  int numberOfColumns = 0;
 	  const char *delimiter = ",";
 	  const char *empty = " ";
       for (i = 0; fgets(buffer, sizeof buffer, file); ++i) {
@@ -32,25 +32,25 @@ int main(void) {
 				  idx++;
 				  printf("%c\n", *s);
 				  if(!strchr(delimiter, *s)) {
-					  if (idx < 2 && strchr(empty, *s)) {
-						  idx = 0;
-					  } else {
-						  headings[headingNumber][idx] = *s;
-					  }
+//					  if (idx < 2 && strchr(empty, *s)) {
+//						  idx = 0;
+//					  } else {
+						  headings[numberOfColumns][idx] = *s;
+//					  }
 				  } else {
 					  printf("comma found\n");
-					  printf("heading %i:", headingNumber + 1);
+					  printf("heading %i:", numberOfColumns + 1);
 					  for (int x = 0; x < idx; x++) {
-						  printf("%c", headings[headingNumber][x]);
+						  printf("%c", headings[numberOfColumns][x]);
 					  }
-					  headingNumber++;
+					  numberOfColumns++;
 					  idx=0;
 				  }
 			  }
     	  } else {
     		  if (i == 1) {
     			  printf("Headings:\n");
-    			  for (int z = 0; z < headingNumber + 1; z++) {
+    			  for (int z = 0; z < numberOfColumns + 1; z++) {
     				  int lspace = 0, rspace = 0;
     				  printf("Heading number %i\n" , z + 1);
     				  for (int s=0; s<10; s++) {
@@ -69,30 +69,34 @@ int main(void) {
     				  }
     				  printf("Space left:%i\n" , lspace);
     				  printf("Space right:%i\n" , rspace);
-//					  for (int x = 1; x < headingLengths[z]; x++) {
-//						  printf("%c", headings[z][x]);
-//					  }
-					  int length = (100 - rspace) - lspace + 1;
+					  int length = (100 - rspace) - lspace + 2;
+					  if (z == numberOfColumns) {
+						  length--; // for the newline
+					  }
 					  char trimmed[length];
 					  int idx = 0;
 					  for (int t = lspace; t < length; t++) {
 						  trimmed[idx] = headings[z][t];
 						  idx++;
 					  }
-					  printf("Trimmed heading: %s\n", trimmed);
-					  printf("Trimmed heading: %s\n", trimmed);
+					  trimmed[idx] = '\0';
 					  printf("Trimmed heading: %s\n", trimmed);
 					  printf("Trimmed heading length: %i", strlen(trimmed));
 					  printf("\n");
 					  printf("Checking if numeric..\n");
 					  printf("\n\n");
-//					  for (i = 0; i < strlen(headings[z][x]); ++i) {
-//					      if (!isdigit(s[i]) && !strchr(empty, *s))
-//					      {
-//					          valid = FALSE;
-//					          break;
-//					      }
-//					  }
+					  int text = 0;
+					  for (i = 0; i < strlen(trimmed); ++i) {
+					      if (!isdigit(trimmed[i])) {
+					          text = 1;
+					          break;
+					      }
+					  }
+					  // example: CREATE TABLE IF NOT EXISTS TTC (id INTEGER PRIMARY KEY,
+					  // Route_ID TEXT, Branch_Code TEXT, Version INTEGER, Stop INTEGER,
+					  // Vehicle_Index INTEGER, Day Integer, Time TEXT)"
+//					  char create[5000];
+
     			  }
     		  }
     		  if (i < 2) {
